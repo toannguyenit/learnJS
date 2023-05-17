18bae656fba6a2ee31fee0b1de0d988bf9d2b7c9
toannguyenit <toannguyen.hcmute@gmail.com>
2023-05-17 10:18:41 +0700

    firts commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/* Javascript */
+// const headingJS = document.getElementsByClassName("heading");
+// console.log(headingJS);
+
+let a = 6;
+a++
+let aPlusPlus = a
+a = a + 1
+
+
+/* JQuery */
+const headingJQ = $(".heading");
+console.log($(".heading"));
+
