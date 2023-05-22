77251018c86cf9530a5df83306ed2254a111ed7f
toannguyenit <toannguyen.hcmute@gmail.com>
2023-05-22 08:49:00 +0700

    push all change

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 /* Javascript */
-// const headingJS = document.getElementsByClassName("heading");
-// console.log(headingJS);
+// var listToDo = document.querySelector(".list-wrapper");
 
-let a = 6;
-a++
-let aPlusPlus = a
-a = a + 1
-
+// var htmlList = `
+//     <ul class="list">
+//         <li class="list-item">a</li>
+//         <li class="list-item">a</li>
+//         <li class="list-item">a</li>
+//         <li class="list-item">a</li>
+//     </ul>
+//  `;
+//  listToDo.innerHTML = htmlList;
 
 /* JQuery */
-const headingJQ = $(".heading");
-console.log($(".heading"));
+// const headingJQ = $(".heading");
+// console.log($(".heading"));
 
+// var htmlList
+
+// function renderList(htmlList) {
+//   $(".list").html(htmlList);
+// }
+
+// $(".btn-add").on("click", function () {
+//   var giatrinhap = $("#input-value").val();
+//   htmlList = htmlList + `<li class="list-item">${giatrinhap} </li>`
+//   renderList(htmlList);
+// });
+var todoList = [
+  {
+    content: "Lam bai tap ve nha",
+    finish: false,
+  },
+];
+var students = {
+  name: "song toan",
+  age: 27,
+  address: "HCM",
+};
+
+console.log(students);
+$(".news").html(students.name);
